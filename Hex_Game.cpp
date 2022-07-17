0d27bf7f2bc84e6b97b33e661e659071c164e89a
Pharia Le <pharia.le@gmail.com>
2022-07-17 15:18:03 -0700

    Adding Descriptor of Initial Project Goal
    Describing what is required in the header of
    of the file

--- Hex_Game.cpp
+++ Hex_Game.cpp
@@ -1,5 +1,9 @@
 // Hex_Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// 
+// Intial Project: Draw a HEX board and determine a legal position and a winning position
+// 
+// Pharia Le
+// 17 July 2022
 
 #include <iostream>
 
